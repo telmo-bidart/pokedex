69cf252966e182b829480047e6d78597f7310bc3
telmo bidart <telmobidart@gmail.com>
2022-01-19 15:36:58 -0300

    layout fixed

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,16 @@
 import Head from 'next/head'
-import React from 'react'
+import React, { Children } from 'react'
 
-const Layout = ({title}) => {
+export default function Layout({ title, children }) {
     return (
         <div className='bg-red-800'>
             <Head>
-                <title>
-                  {title}
-                </title>
+                <title>{title}</title>
             </Head>
-            <main className='container mx-auto max-w-xl pt-8 min-h-screen'>
 
+            <main className='container mx-auto max-w-xl pt-8 min-h-screen'>
+            {children}
             </main>
         </div>
     )
 }
-export default Layout
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,45 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Layout from '../components/Layout'
+import Head from "next/head";
+import Image from "next/image";
+import Layout from "../components/Layout";
+import Link from "next/link";
 
-export default function Home() {
+export default function Home({ pokemon }) { 
   return (
     <Layout title="Pokedex">
-        <h1>NextJS pokedex</h1>
+      <h1 className="text-4xl mb-8 text-center">POKEDEX</h1>
+      <ul>{pokemon.map(( pokeman, index) => (
+        <li key={index}>
+          <Link href={`pokemon?id=${index +1}`}>
+            <a>
+              <img src={pokeman.image} alt={pokeman.name} />
+              <span>
+                {index + 1}.
+              </span>
+              {pokeman.name}
+            </a>
+          </Link>
+        </li>
+      ))}</ul>
     </Layout>
-  )
+  );
+}
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    const { results } = await res.json();
+    const pokemon = results.map((result, index) => {
+      const paddedIndex = ("00" + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${paddedIndex}.png`;
+      return {
+        ...result,
+        image,
+      };
+    });
+    return {
+      props: { pokemon },
+    };
+  } catch (err) {
+    console.error(err);
+  }
 }
